61efae9f74c3b55c9d3520e1cb582bbf1a3e5f4e
olaoyesalem <olaoyesalemgreat@gmail.com>
2023-07-09 10:56:09 +0000

    turned it totsx

---
+++ src/scenes/Account/AccountCreate.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+//import AccountCreate from './AccountCreate';
+
+const AccountCreate: React.FC = () => {
+
+    const createAccount=()=>{
+
+    };
+  return (
+    <div>
+      <button onClick={createAccount}>Create Account</button>
+    </div>
+  );
+};
+
+export default AccountCreate;
